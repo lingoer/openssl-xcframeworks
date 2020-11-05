8af72b6e8db2c84be114f5a7dc477c7d9355bf1f
Roy Fu <lingoerer@gmail.com>
2020-11-05 18:47:28 +0800

    chore: expose Swift Package and XCFramework

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "OpenSSL",
+    products: [
+        .library(
+            name: "OpenSSL",
+            targets: ["OpenSSL"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .binaryTarget(name: "OpenSSL", path: "frameworks/openssl.xcframework")
+    ]
+)
